0d760fa427cf9fc7393090a13434ebe9c3094cd4
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2019-11-15 16:42:35 +0000

    Fixed tests

--- build.sbt
+++ build.sbt
@@ -7,5 +7,5 @@ scalaVersion := "2.12.10"
 
 libraryDependencies ++= Seq(
   "log4j" % "log4j" % "1.2.17",
-  "org.scalatest" % "scalatest_2.12" % "3.2.0-M1" % "test"
+  "org.scalatest" % "scalatest_2.12" % "3.0.1" % "test"
 )
--- src/test/scala/AppSuite.scala
+++ src/test/scala/AppSuite.scala
@@ -1,10 +1,22 @@
-import org.scalatest.FunSuite
+import collection.mutable.Stack
+import org.scalatest._
 
-class AppSuite extends FunSuite {
+import scala.collection.mutable
 
-  test("App makeMessage should return the name of the main method") {
-    val result = App.makeMessage()
-    assert(result == "App.main()")
+class AppSuite extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new mutable.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
   }
 
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a [NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
 }
